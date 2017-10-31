4e2a2f2fbffc67f11821571a2b3e704f0ec04592
Keshav Saharia <keshav@techlabeducation.com>
2017-10-31 02:46:00 -0500

    updated

--- README.md
+++ README.md
@@ -1,12 +1,4 @@
 # Kitafi
 
-Kitafi is an open-source approach to teaching the Swahili language. It is a portmanteau of the
-Swahili word **kitabu** (book) and **tafiti** (to look up). Along with
-
-## Contributing
-
-The entire kitafi website is contained in a Github Pages repository.
-
-```
-git clone https://github.com/kitafi/kitafi.github.io
-```
+Kitafi is an open-source approach to teaching the Swahili language. It is a loose portmanteau of the
+Swahili word **kitabu** (book) and **tafiti** (to look up). This repository
---
+++ js/lesson.js
@@ -0,0 +1,63 @@
+$(function() {
+	$('#lesson').carousel({
+		interval: false,
+		wrap: false
+	});
+
+	var slideTypes = ['interactive', 'question', 'learn'];
+
+	$('#lesson').on('slide.bs.carousel', function() {
+		setTimeout(function() {
+			var type = $('#lesson').find('.carousel-item.active').last().data('type');
+			$('body').removeClass(slideTypes.join(' '));
+			$('body').addClass(type);
+		}, 700);
+	});
+
+	$('body').addClass($('.carousel-item').first().data('type'));
+
+	function setInput(input, value) {
+		if (value != null)
+			input.val(value);
+		input.attr('size', input.val().length + 1);
+		if (input.css('width') != 'auto')
+			setTimeout(function() {
+				input.css('width', 'auto');
+			}, 100);
+	};
+
+	$('.carousel-item').each(function() {
+		var item = $(this);
+		var type = item.data('type');
+
+		// Ensure the input resizes with the text content
+		item.find('input').each(function() {
+			var input = $(this);
+			input.keydown(function() {
+				setInput(input);
+			});
+		});
+
+		if (type == 'interactive') {
+			var inputMap = {};
+
+			item.find('input').each(function() {
+				var input = $(this);
+				var name = input.attr('name');
+				if (! inputMap[name])
+					inputMap[name] = [];
+				inputMap[name].push(input);
+
+				input.keyup(function() {
+					inputMap[name].forEach(function(updateInput) {
+						if (! updateInput.is(':focus'))
+							setInput(updateInput, input.val());
+					});
+				});
+			});
+		}
+		else {
+
+		}
+	});
+});
